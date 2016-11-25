ab559e7f3e2c2881d036d98159fa741828c2e67e
Rico Herlt <rherlt@h3ko.de>
2016-11-25 23:20:34 +0100

    - added aspnet core project to azure

---
+++ src/GoodVibrations.Web/BasicAuthentication/UserManagerBasicAuthenticationAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using GoodVibrations.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace GoodVibrations.Web.BasicAuthentication
+{
+    public class UserManagerBasicAuthenticationAttribute : BasicAuthenticationBaseAttribute
+    {
+
+        protected override bool Authorize(ActionExecutingContext actionExecutingContext, NetworkCredential credential)
+        {
+            var isValid = false;
+            var manager = actionExecutingContext.HttpContext.RequestServices.GetService(typeof (UserManager<ApplicationUser>)) as UserManager<ApplicationUser>;
+            if (manager != null)
+            {
+                var user = manager.FindByEmailAsync(credential.UserName).Result;
+                isValid = manager.CheckPasswordAsync(user, credential.Password).Result;
+            }
+
+            return isValid;
+        }
+    }
+}
