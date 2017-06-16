8e0d425e0b3c4bfe1b4d9499382c14177d1fd5c6
Rico Herlt <rherlt@h3ko.de>
2017-06-16 09:18:46 +0200

    added signal-r testclinent

---
+++ src/GoodVibrations.SignalRTestClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GoodVibrations.SignalRTestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string url = "http://goodvibrations-dev.azurewebsites.net/";
+            const string url2 = "http://localhost:6169//signalr";
+            while (true)
+            {
+                Console.WriteLine("SignalR Testclient.");
+                Console.WriteLine($"Connect To: {url}");
+               
+                try
+                {
+                    var client = new SignalRClient(url);
+                    client.ConnectToSignalRHub().GetAwaiter().GetResult();
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"{DateTime.Now:O}\tError: {e}");
+                    Console.Write("\r\n\r\nPress ENTER to retry. Press CTRL+C to exit the application!\r\n\r\n");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                Console.Write("\r\n\r\nPress CTRL+C to exit the application!\r\n\r\n");
+                //run forever 
+                while (true)
+                    Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ src/GoodVibrations.SignalRTestClient/SignalRClient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace GoodVibrations.SignalRTestClient
+{
+    class SignalRClient
+    {
+        private readonly string _url;
+
+        public SignalRClient(string url)
+        {
+            _url = url;
+        }
+
+        public async Task ConnectToSignalRHub()
+        {
+            var hubConnection = new HubConnection(_url);
+            var locationPrody = hubConnection.CreateHubProxy("NotifyHub");
+            locationPrody.On<string>("Notify", eventId =>
+            {
+                Console.WriteLine($"SignalR notification recieved: {eventId}");
+            });
+
+            //wire up all events
+            hubConnection.StateChanged +=
+                change => Console.WriteLine($"{DateTime.Now:O}\tState Changed! Old State: {change.OldState} New State: {change.NewState}");
+            hubConnection.Closed += () => Console.WriteLine($"{DateTime.Now:O}\tConnection Closed");
+            hubConnection.ConnectionSlow += () => Console.WriteLine($"{DateTime.Now:O}\tConnection Slow");
+            hubConnection.Reconnected += () => Console.WriteLine($"{DateTime.Now:O}\tReconnected");
+            hubConnection.Error += exception => Console.WriteLine($"{DateTime.Now:O}\tError: {exception}");
+            hubConnection.Reconnecting += () => Console.WriteLine($"{DateTime.Now:O}\tReconnecting");
+            //hubConnection.Received += s => Console.WriteLine($"{DateTime.Now:O}\tReceived: {s}");
+
+
+            //start connection to hub
+            await hubConnection.Start();
+            Console.WriteLine($"{DateTime.Now:O}\tHubConnection Started!");
+        }
+    }
+}
